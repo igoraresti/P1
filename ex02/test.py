d1c8208ed7cb76c216df6061157d5859cd3a4a20
sibori90 <igoraresti@gmail.com>
2016-03-31 11:08:36 +0200

    ficheros ex02 borrados

--- ex02/test.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-cmd = "gcc ft_print_alphabet.c test2.c -o programa"
-subprocess.call(cmd, shell = True)
-cmd = "./programa"
-pipe = subprocess.Popen(cmd, shell = True, stdout=subprocess.PIPE,
-                                           stderr=subprocess.PIPE)
-out, err = pipe.communicate()
-print "el output del programa es: "+out
